fc0e40f80ec6119ec9ee51beded04ad2b639a552
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-10 20:51:54 +0300

    Introduction to mongoose

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('Hello, MEAN!');
+var mongoose = require('mongoose');
+var schema = require('./schema');
+
+mongoose.connect('mongodb://localhost:27017/edxMEAN');
+
+// (model name, schema, collection name)
+var User = mongoose.model('User', schema, 'users');
+
+var user = new User({
+  name: 'John Smith',
+  email: 'john@smith.io'
+});
+
+user.save(function(error) {
+  if (error) { 
+    console.log(error);
+    process.exit(1);
+  }
+  User.find({}, function(error, docs){
+    if (error) { 
+      console.log(error);
+      process.exit(1);
+    }
+    console.log(docs);
+    process.exit(0);
+  });
+});
