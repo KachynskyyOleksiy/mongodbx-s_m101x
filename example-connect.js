d894155a67549ca903bf7fa84b7091be0c70739a
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-10 13:00:45 +0300

    add movies, learn queries

--- example-connect.js
+++ example-connect.js
@@ -7,23 +7,73 @@ mongodb.MongoClient.connect(uri, function(error, db) {
     process.exit(1);
   }
 
-  db.collection('sample').insert({ x: 1 }, function(error, result) {
+  var movies = [
+  {
+    "title": "Dawn of the Planet of the Apes",
+    "image": "http://api.androidhive.info/json/movies/1.jpg",
+    "ratings": {
+      "site": 8.3,
+      "my": 8
+    },
+    "releaseYear": 2014,
+    "genre": ["Action", "Drama", "Sci-Fi"]
+  },
+  {
+    "title": "District 9",
+    "image": "http://api.androidhive.info/json/movies/2.jpg",
+    "ratings": {
+      "site": 8,
+      "my": 7
+    },
+    "releaseYear": 2009,
+    "genre": ["Action", "Sci-Fi", "Thriller"]
+  },
+  {
+    "title": "X-Men: Days of Future Past",
+    "image": "http://api.androidhive.info/json/movies/4.jpg",
+    "ratings": {
+      "site": 8.4,
+      "my": 5
+    },
+    "releaseYear": 2014,
+    "genre": ["Action", "Sci-Fi", "Thriller"]
+  }
+  ];
+
+  db.collection('movies').insert(movies, function(error, result) {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  });
+
+  var query1 = { releaseYear: 2009};
+  db.collection('movies').find(query1).toArray(function(error, docs) {
     if (error) {
       console.log(error);
       process.exit(1);
     }
 
-    db.collection('sample').find().toArray(function(error, docs) {
-      if (error) {
-        console.log(error);
-        process.exit(1);
-      }
-
-      console.log('Found docs:');
-      docs.forEach(function(doc) {
-        console.log(JSON.stringify(doc));
-      });
-      process.exit(0);
+    console.log('Found docs (query1):');
+    docs.forEach(function(doc) {
+      console.log(doc);
     });
+    // process.exit(0);
   });
+
+  var query2 = { genre: 'Action', 'ratings.my': { '$gte': 6} };
+  db.collection('movies').find(query2).toArray(function(error, docs) {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+
+    console.log('Found docs (query2):');
+    docs.forEach(function(doc) {
+      console.log(doc);
+    });
+
+    // process.exit(0);
+  });
+
 });
